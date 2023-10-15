f578704134840c486e47ca809517690fa48d80d4
Axel <axel0064@gmail.com>
2023-10-15 20:28:11 -0300

    correccion de errores

--- js/cardService.js
+++ js/cardService.js
@@ -1,49 +1,56 @@
-function agregarAlCarrito(producto){
+function agregarAlCarrito(producto) {
     const memoria = JSON.parse(localStorage.getItem("articulos"));
     console.log(memoria);
 
-    if(!memoria){
-        const nuevoArticulo = producto; //cambiar nuevoarticulo por nuevoproducto
-        nuevoArticulo.cantidad=1;
-        localStorage.setItem("articulos", JSON.stringify([nuevoArticulo]))
-    } 
-    else{
+    if (!memoria) {
+        const nuevoArticulo = producto;
+        nuevoArticulo.cantidad = 1;
+        localStorage.setItem("articulos", JSON.stringify([nuevoArticulo]));
+    } else {
         const indiceProducto = memoria.findIndex(articulos => articulos.id === producto.id);
         console.log(indiceProducto)
         const nuevaMemoria = memoria;
-        if(indiceProducto === -1){
+        if (indiceProducto === -1) {
             nuevaMemoria.push(getNuevoProductoParaMemoria(producto));
-        }else{
-            nuevaMemoria[indiceProducto].cantidad ++;
+        } else {
+            nuevaMemoria[indiceProducto].cantidad++;
         }
         localStorage.setItem("articulos", JSON.stringify(nuevaMemoria));
     }
     actualizarNumeroCarrito();
+    crearTarjetasProductos(); // Actualiza las tarjetas después de agregar
 }
 
-function restarAlCarrito(producto){
+function restarAlCarrito(producto) {
     const memoria = JSON.parse(localStorage.getItem("articulos"));
     const indiceProducto = memoria.findIndex(articulos => articulos.id === producto.id);
-    if (memoria[indiceProducto].cantidad === 1){
-        memoria.splice(indiceProducto,1);
-    }else{
+    if (memoria[indiceProducto].cantidad === 1) {
+        memoria.splice(indiceProducto, 1);
+    } else {
         memoria[indiceProducto].cantidad--;
     }
     localStorage.setItem("articulos", JSON.stringify(memoria));
+    actualizarNumeroCarrito();
+    crearTarjetasProductos(); // Actualiza las tarjetas después de restar
 }
 
-function getNuevoProductoParaMemoria(producto){
+function getNuevoProductoParaMemoria(producto) {
     const nuevoProducto = producto;
     nuevoProducto.cantidad = 1;
     return nuevoProducto;
 }
 
-
 const contadorCarritoElement = document.getElementById("contadorCarrito");
-function actualizarNumeroCarrito(){
+
+function actualizarNumeroCarrito() {
     const memoria = JSON.parse(localStorage.getItem("articulos"));
-    const contador = memoria.reduce((acum, current) => acum+current.cantidad,0 );
-    contadorCarritoElement.innerText = contador;
+    if (contadorCarritoElement) {
+        const contador = memoria.reduce((acum, current) => acum + current.cantidad, 0);
+        contadorCarritoElement.innerText = contador;
+    }
 }
 
-actualizarNumeroCarrito();
+// Asegúrate de que el documento se ha cargado antes de llamar a actualizarNumeroCarrito
+document.addEventListener("DOMContentLoaded", function() {
+    actualizarNumeroCarrito();
+});

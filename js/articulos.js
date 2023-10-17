a88aeb5abe2fe380da6b1f8d5f3626d6a9a38e42
Axel <axel0064@gmail.com>
2023-10-16 23:02:13 -0300

    correccion de imagenes

--- js/articulos.js
+++ js/articulos.js
@@ -3,40 +3,40 @@ const articulos = [
     {
         id: 1,
         nombre: "Abrigo marron",
-        imagen: "./assets/img/Ropa/ropa1.png",
+        imagen: "./assets/img/Ropa/ropa1.jpg",
         precio: 400,
         categoria: "Abrigos"
     },
     {
         id: 2,
         nombre: "Remera basica",
-        imagen: "./assets/img/Ropa/ropa2.png",
+        imagen: "./assets/img/Ropa/ropa2.jpg",
         precio: 200,
     },
     {
         id: 3,
         nombre: "Gorro de lana",
-        imagen: "./assets/img/Ropa/ropa3.png",
+        imagen: "./assets/img/Ropa/ropa3.jpg",
         precio: 110,
     },
     {
         id: 4,
         nombre: "Chaqueta de Cuero Negra",
-        imagen: "./assets/img/Ropa/ropa4.png",
+        imagen: "./assets/img/Ropa/ropa4.jpg",
         precio: 450,
         categoria: "Chaquetas"
     },
     {
         id: 5,
         nombre: "Pantalones Vaqueros Clásicos",
-        imagen: "./assets/img/Ropa/ropa5.png",
+        imagen: "./assets/img/Ropa/ropa5.jpg",
         precio: 250,
         categoria: "Pantalones"
     },
     {
         id: 6,
         nombre: "Bufanda de Lana Tejida",
-        imagen: "./assets/img/Ropa/ropa6.png",
+        imagen: "./assets/img/Ropa/ropa6.jpg",
         precio: 35,
         categoria: "Accesorios"
     }
